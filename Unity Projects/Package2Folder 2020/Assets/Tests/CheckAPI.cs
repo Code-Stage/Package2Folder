f043a98a75849f3686a0043ce1ae810c09b581dd
Dmitriy Yukhanov <focus@codestage.net>
2021-08-20 23:31:52 +0300

    Add test projects (beware: symlinks are used)

---
+++ Unity Projects/Package2Folder 2020/Assets/Tests/CheckAPI.cs
@@ -0,0 +1,11 @@
+﻿using CodeStage.PackageToFolder;
+using UnityEditor;
+
+public class CheckAPI 
+{
+	[MenuItem("Tools/Test")]
+	public static void Test()
+	{
+		Package2Folder.ImportPackageToFolder(@"D:\1.unitypackage", @"Assets/Wow", false);
+	}
+}
---
+++ Unity Projects/Package2Folder Master/Assets/Tests/CheckAPI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace CodeStage.PackageToFolder.Tests
+{
+	public static class CheckAPI
+	{
+		[MenuItem("Tools/Test")]
+		public static void Test()
+		{
+			Package2Folder.ImportPackageToFolder(@"D:\1.unitypackage", @"Assets/Wow", false);
+		}
+	}
+}
